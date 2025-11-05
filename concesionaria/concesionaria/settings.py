8525a8208a34d8ef54d4bed5ba4f4053703f1d95
Matiaslagost7 <matiaslagost7@gmail.com>
2025-11-05 16:45:15 -0300

    Se creo la conexion a postgres

--- concesionaria/concesionaria/settings.py
+++ concesionaria/concesionaria/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'concesionaria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'concesionaria_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
